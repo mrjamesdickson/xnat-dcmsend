c32f63cbcd7eba6cb6cf828172c242a0c6569516
James Dickson <mrjamesdickson@gmail.com>
2025-05-12 13:05:27 -0400

    some minor command.json changes

--- build_hub.sh
+++ build_hub.sh
@@ -1,2 +1,3 @@
-docker build -t xnatworks/xnat-dcmsend:1.0 .
-docker push xnatworks/xnat-dcmsend:1.0
+python3 ./command2label.py command.json >> Dockerfile
+docker build -t xnatworks/xnat-dcmsend:1.1.0 .
+docker push xnatworks/xnat-dcmsend:1.1.0
